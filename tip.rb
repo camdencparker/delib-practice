495faebb9471c0d30c930476bca9ccfbf4b72caa
Camden <camdencparker@gmail.com>
2022-04-05 15:21:10 -0700

    FizzBuzz wb

--- tip.rb
+++ tip.rb
@@ -60,3 +60,33 @@
   end
 
  p less_one_hundred([1000, 1, 100, -10, 10000, 0])
+
+
+#  Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+ # create a function that takes in the argument of n 
+ # us the map method to create block variable and returns array
+  # if num is divisible by 3 and by 5 return FizzBuzz
+  # elsif num is divisible by 3 return Fizz
+  # elsif num is divisible by just 5 return Buzz
+  # else return the num 
+
+ def fizz_buzz(n)
+  (1..n).map do |num|
+      if num % 3 == 0 && num % 5 == 0
+            "FizzBuzz"
+      elsif num % 3 == 0
+            "Fizz"
+      elsif num % 5 == 0
+            "Buzz"
+      else
+           num
+     end
+ end
+end
+
+p fizz_buzz(15)
