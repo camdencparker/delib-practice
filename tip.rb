6b3ab9a6447f986e81f0c3464e4aacb3d0a59082
Camden <camdencparker@gmail.com>
2022-04-16 19:19:45 -0700

    prime wb

--- tip.rb
+++ tip.rb
@@ -359,3 +359,11 @@ def fibo()
 end
 
 p fibo()
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+require 'prime'
+number = 13256 
+p Prime.each(number).select{ |n| number % n == 0 }.last
